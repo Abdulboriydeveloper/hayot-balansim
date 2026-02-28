9016b4890931fb1b16d06458fa9310d11360fa3b
Abdulboriy <84060501+Abdulboriydeveloper@users.noreply.github.com>
2026-02-28 16:31:16 +0500

    fix popup auto-open and double popup

--- src/UmidHudoyberdievVSL.jsx
+++ src/UmidHudoyberdievVSL.jsx
@@ -19,11 +19,9 @@ const CREDS = [
   { t: "4 yil ichida 34,000+ tadan ortiq o'quvchilarimga shaxsiy rivojlanishga yordam bergan", gold: false },
 ];
 
-// Popupni ochish - index.html dagi funksiya
-function openAmoForm() {
-  if (window.openAmoForm) {
-    window.openAmoForm();
-  }
+// ODDIY FUNKSIYA - faqat linkga o'tadi
+function goToForm() {
+  window.location.href = "https://forms.amocrm.ru/rczcdxl";
 }
 
 function InstaBadge() {
@@ -64,11 +62,11 @@ function Hero() {
           <p className="sm-hero-sub sfu d2">
             Qanday qilib xotirangizni 10 barobarga kuchaytirish va istalgan ma'lumotlarni eslab qolish mumkin?
           </p>
-          <button className="sm-btn sfu d3" onClick={openAmoForm}>
+          <button className="sm-btn sfu d3" onClick={goToForm}>
             Ro'yxatdan o'tish
           </button>
           <div className="sfu d4">
-            <CtaLink onClick={openAmoForm} />
+            <CtaLink onClick={goToForm} />
           </div>
         </div>
       </div>
@@ -95,8 +93,8 @@ function Benefits() {
           <div className="sm-card red"><div className="sm-card-imgbox tall"><img src={card5} alt="Sirli bonuslar" /></div><div className="sm-card-body"><p>Hech qayerda berilmagan<br /><strong>sirli bonuslar</strong> (sizga yoqishi aniq)</p></div></div>
         </div>
         <div className="sm-bcta">
-          <button className="sm-btn" onClick={openAmoForm}>Ro'yxatdan o'tish</button>
-          <CtaLink onClick={openAmoForm} />
+          <button className="sm-btn" onClick={goToForm}>Ro'yxatdan o'tish</button>
+          <CtaLink onClick={goToForm} />
         </div>
       </div>
     </section>
@@ -117,8 +115,8 @@ function Author() {
           <div className="sm-author-right">
             <img className="sm-author-photo" src={authorPhoto} alt="Umidjon Hudoyberdiev" />
             <div className="sm-author-cta">
-              <button className="sm-btn" onClick={openAmoForm}>Ro'yxatdan o'tish</button>
-              <CtaLink onClick={openAmoForm} />
+              <button className="sm-btn" onClick={goToForm}>Ro'yxatdan o'tish</button>
+              <CtaLink onClick={goToForm} />
             </div>
           </div>
         </div>
